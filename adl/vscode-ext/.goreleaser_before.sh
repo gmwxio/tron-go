a23347276ab65bb1523d72d9b2127f3d33b0bf14
Gary Miller <gm@wx.io>
2019-07-02 20:45:00 +1000

    wip auto version on goreleaser

--- adl/vscode-ext/.goreleaser_before.sh
+++ adl/vscode-ext/.goreleaser_before.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-rm *.vsix || true
+rm *.vsix 2> /dev/null || true
+
+TAG=`git tag -l --points-at HEAD`
+if [ "$TAG" != "" ]; then
+    T2=$(echo $TAG | sed 's/v\(.*\)/"\1"/')
+    sed -i 's!"version":.*,!"version": '"$T2"',!' package.json
+    sed -i 's!"version":.*,!"version": '"$T2"',!' client/package.json
+fi
